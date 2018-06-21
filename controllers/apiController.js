e6f9f2e1151dd3241b899a52069424b41366e370
github username <github email address>
2018-06-20 22:26:10 -0400

    built all api routes

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,4 +1,4 @@
-var Todos = require('..models/todoModel')
+var Todos = require('../models/todoModel')
 var bodyParser = require('body-parser')
 
 module.exports = function(app) {
@@ -6,13 +6,14 @@ module.exports = function(app) {
   app.use(bodyParser.urlencoded({ extended: true }))
 
   app.get('/api/todos/:username', function(req, res) {
-    Todos.find({ username: req.params.username }, function(err, todos) {
-      if (err) {
-        throw err
-      }
-      res.send(todos)
-    })
-  })
+
+      Todos.find({ username: req.params.username }, function(err, todos) {
+          if (err) throw err;
+
+          res.send(todos);
+      });
+
+  });
 
   app.get('api/todo/:id', function(req, res) {
     Todos.findById({ _id: req.params.id }, function(err, todo) {
